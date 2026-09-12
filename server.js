6b6a54004bcc0f4d8740c406a596322b68dfb97c
sestoses <uzunayserkan@yandex.com>
2026-09-12 17:57:17 +0300

    Initialize package.json for WebRTC PTT Server

---
+++ server.js
@@ -0,0 +1,12 @@
+{
+  "name": "sestoses",
+  "version": "1.0.0",
+  "description": "WebRTC PTT Server",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "ws": "^8.16.0"
+  }
+}
